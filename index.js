eebed055dd49d81ee99992a42cf5d75a214dad8c
unknown <islam15-3879@diu.edu.bd>
2022-04-29 22:56:46 +0600

    finished

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT||5000;
 require('dotenv').config()
@@ -21,19 +21,44 @@ async function run(){
         const productCollection =client.db('emaJohn').collection('product');
 
         app.get('/product',async(req,res)=>{
+          console.log('query',req.query);
+          const page =parseInt(req.query.page);
+          const size=parseInt(req.query.size);
             const query ={};
             const cursor =productCollection.find(query);
-            const products = await cursor.toArray();
+            let products;
+            if(page|| size){
+              // 0-->skip: 0 get:0-10
+              // 1-->skip: 1 get:11-20
+              // 2-->skip: 3 get:21-30
+              products = await cursor.skip(page*size).limit(size).toArray();
+
+            }
+            else{
+              products = await cursor.toArray();
+
+            }
+
             res.send(products);
         })
         app.get('/productCount', async(req,res)=>{
-            const query ={};
-            const cursor =productCollection.find(query);
-            const count =await cursor.count();
+            // const query ={};
+            // const cursor =productCollection.find(query);
+            const count =await productCollection.estimatedDocumentCount();
             // res.json(count);
             res.send({count});
             // ekahne object dici karon nahoy string hisebe jabe na 
 
+        })
+        //use post to get products by ids 
+        app.post('/productByKeys',async(req,res)=>{
+          const keys =req.body;
+          const ids =keys.map(id=>ObjectId(id))
+          const query ={_id:{$in: ids}}
+          const cursor =productCollection.find(query);
+          const products=await cursor.toArray();
+          console.log(keys);
+          res.send(products);
         })
     }
     finally{
