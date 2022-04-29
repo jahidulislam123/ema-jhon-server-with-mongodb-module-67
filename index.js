7c0ff3fe9d243550280a60430bd81db46baa3cb3
unknown <islam15-3879@diu.edu.bd>
2022-04-29 12:08:56 +0600

    half-added

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require('express');
 const cors =require('cors');
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port =process.env.PORT||5000;
+require('dotenv').config()
 
 // middlewire 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umu1r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection =client.db('emaJohn').collection('product');
+
+        app.get('/product',async(req,res)=>{
+            const query ={};
+            const cursor =productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+        app.get('/productCount', async(req,res)=>{
+            const query ={};
+            const cursor =productCollection.find(query);
+            const count =await cursor.count();
+            // res.json(count);
+            res.send({count});
+            // ekahne object dici karon nahoy string hisebe jabe na 
+
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('John is running  and waiting for ema!')
